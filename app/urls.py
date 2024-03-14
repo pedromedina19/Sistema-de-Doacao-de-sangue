503ae10aa9b20605d485d4d61bdebd2260b3bd44
Bruno Henrique <brunooliveira598@gmail.com>
2024-03-14 10:52:13 -0300

    modificação da view 'teste' e do arquivo 'index'

--- app/urls.py
+++ app/urls.py
@@ -6,8 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('teste/', views.teste, name='teste'),
-    path('teste2/<int:valor>/', views.teste2, name='teste2'),
-    path('teste3/', views.teste3, name='teste3'),
-    path('teste4/', views.teste4, name='teste4'),
+    path('teste/<int:valor>/', views.teste, name='teste_com_parametro'),
 ]
 
--- app/views.py
+++ app/views.py
@@ -6,23 +6,15 @@ def home(request):
   return render(request, "app/index.html")  
   
 
-def teste(request):
-  # return render(request, "app/teste.html")
-  return HttpResponse("Rota executou com sucesso!")
+def teste(request, valor=None):    
+  if valor is not None:
+    return HttpResponse(f'Rota executou com sucesso recebendo o valor {valor}!')  
 
-def teste2(request, valor):
-  return HttpResponse(f'Rota executou com sucesso recebendo o valor {valor}!')
-  # return JsonResponse({'message': response_message})
-  
-
-def teste3(request):
-  if request.method == 'POST':
-    valor = request.POST.get('valor') 
-    return HttpResponse(f'Rota executou com sucesso recebendo o valor {valor}!')
-
-def teste4(request):  
-  valor = request.GET.get('valor')  
+  valor_query = request.GET.get('valor')  
   quantidade = request.GET.get('quantidade')
-  return HttpResponse(f'Rota executou com sucesso recebendo o valor {valor} e quantidade {quantidade}!')
+  
+  if valor_query and quantidade:
+    return HttpResponse(f'Rota executou com sucesso recebendo o valor {valor_query} e quantidade {quantidade}!')
 
+  return HttpResponse("Rota executou com sucesso!")
   
