1003133f26d4a2a85be3057ac69a97d642e4bbb0
Bruno Henrique <brunooliveira598@gmail.com>
2024-04-02 18:24:50 -0300

    funções de validação no arquivo forms

--- app_doador/forms.py
+++ app_doador/forms.py
@@ -8,4 +8,22 @@ class Form(forms.ModelForm):
         fields = '__all__'
 
 
+    def clean_booleano(self):        
+        booleano = self.cleaned_data.get("booleano")
+
+        if booleano == False:
+            raise ValidationError('Checkbox deve ser marcado.')
+        
+        return booleano
+        
+
+    def clean_opcaoSelect(self):
+        opcaoSelect = self.cleaned_data.get("opcaoSelect")
+
+        if opcaoSelect == 'empty':
+            raise ValidationError('Selecione uma opção.')
+
+        return opcaoSelect
   
+
+    
